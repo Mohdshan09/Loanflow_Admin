1bbdee58a28471aeeecfb5effba748b4c6298525
Mohammad Shan <mohdshan1024@gmail.com>
2026-09-15 22:21:34 +0530

    inital backend server created

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,19 @@
-// Express app configuration and middleware.\n
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//health-check
+app.get("/", (_req, res) => {
+    res.json({
+        message: "API is working !"
+    })
+});
+
+export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,9 @@
-// HTTP server bootstrap.\n
+import "dotenv/config"
+import app from "./app.js"
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`server listening at port: ${PORT}`)
+})
+
